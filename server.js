f7701ac0dc184c8986e61284a397d1597bc705a2
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-05-02 16:42:59 -0400

    fixed syntax error

--- server.js
+++ server.js
@@ -11,43 +11,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const port = args.port || 5000;
 
-app.get('/app/', (req, res) {
+app.get('/app/', (req, res) => {
 	res.status(200).send("200 OK");
 });
 
-app.get('/app/rps/', (req, res) {
+app.get('/app/rps/', (req, res) => {
 	res.status(200).send(rps());
 });
 
-app.get('/app/rpsls/', (req, res) {
+app.get('/app/rpsls/', (req, res) => {
 	res.status(200).send(rpsls());
 });
 
-app.post('/app/rps/play/', (req,res) {
+app.post('/app/rps/play/', (req,res) => {
 	res.status(200).send(rps(req.body.shot));
 });
 
-app.post('/app/rpsls/play/', (req,res) {
+app.post('/app/rpsls/play/', (req,res) => {
 	res.status(200).send(rpsls(req.body.shot));
 });
 
-app.get('/app/rps/play/', (req,res) {
+app.get('/app/rps/play/', (req,res) => {
 	res.status(200).send(rps(req.query.shot));
 });
 
-app.get('/app/rpsls/play/', (req,res) {
+app.get('/app/rpsls/play/', (req,res) => {
 	res.status(200).send(rps(req.query.shot));
 });
 
-app.get('/app/rpsls/play/(rock|paper|scissors)/', (req,res) {
+app.get('/app/rpsls/play/(rock|paper|scissors)/', (req,res) => {
 	res.status(200).send(rps(req.params.shot));
 });
 
-app.get('/app/rpsls/play/(rock|paper|scissors|lizard|spock)/', (req,res) {
+app.get('/app/rpsls/play/(rock|paper|scissors|lizard|spock)/', (req,res) => {
 	res.status(200).send(rpsls(req.params.shot));
 });
 
-app.get('*', (req,res) {
+app.get('*', (req,res) => {
 	res.status(404).send('404 NOT FOUND')
 });
 
