973276a67ba096c34d60fa3be0a9e7ff7ee582d2
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2023-05-02 15:17:35 -0400

    Allows input and can display json or urlencoded

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ import minimist from 'minimist';
 import express from 'express';
 
 import {rps, rpsls } from './lib/rpsls.js';
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
